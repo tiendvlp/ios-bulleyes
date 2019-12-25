1fe917c389e475c76ac8c90f3ce14577fb1120ed
MinTin <tiendvlp@gmail.com>
2019-12-25 16:35:01 +0700

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Đặng Tiến on 12/25/19.
+//  Copyright © 2019 Mintin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
